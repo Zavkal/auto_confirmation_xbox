45c1bd6640f164ae84d14cd06ea1d36b6e1c63a3
Zavkal <ivanovilya14122000@yandex.ru>
2025-06-07 15:50:17 +0700

    Закомментировал

--- rabbit/send_result.py
+++ rabbit/send_result.py
@@ -26,36 +26,36 @@ def send_result_message(result_data) -> None:
     logger.info(f"[>] Отправил результат: {result_data}")
     connection.close()
 
-class AccessRequestPublisher:
-    def __init__(self, host: str, user: str, password: str, queue_name: str) -> None:
-        self.rabbitmq_host = host
-        self.rabbitmq_user = user
-        self.rabbitmq_pass = password
-        self.queue_name = queue_name
-
-    def _connect(self):
-        connection = pika.BlockingConnection(
-            pika.ConnectionParameters(
-                host=self.rabbitmq_host,
-                credentials=pika.PlainCredentials(self.rabbitmq_user, self.rabbitmq_pass),
-            )
-        )
-        channel = connection.channel()
-        channel.queue_declare(queue=self.queue_name)
-        return connection, channel
-
-    def publish(self, *, entity: AccessRequestQueueEntity) -> None:
-        body = json.dumps(asdict(entity)).encode("utf-8")
-
-        connection, channel = self._connect()
-        try:
-            channel.basic_publish(
-                exchange="",
-                routing_key=self.queue_name,
-                body=body,
-                properties=pika.BasicProperties(delivery_mode=1),
-            )
-            logger.info(f"Отправлено сообщение в очередь -> {self.queue_name}: {asdict(entity)}")
-        finally:
-            channel.close()
-            connection.close()
+# class AccessRequestPublisher:
+#     def __init__(self, host: str, user: str, password: str, queue_name: str) -> None:
+#         self.rabbitmq_host = host
+#         self.rabbitmq_user = user
+#         self.rabbitmq_pass = password
+#         self.queue_name = queue_name
+#
+#     def _connect(self):
+#         connection = pika.BlockingConnection(
+#             pika.ConnectionParameters(
+#                 host=self.rabbitmq_host,
+#                 credentials=pika.PlainCredentials(self.rabbitmq_user, self.rabbitmq_pass),
+#             )
+#         )
+#         channel = connection.channel()
+#         channel.queue_declare(queue=self.queue_name)
+#         return connection, channel
+#
+#     def publish(self, *, entity: AccessRequestQueueEntity) -> None:
+#         body = json.dumps(asdict(entity)).encode("utf-8")
+#
+#         connection, channel = self._connect()
+#         try:
+#             channel.basic_publish(
+#                 exchange="",
+#                 routing_key=self.queue_name,
+#                 body=body,
+#                 properties=pika.BasicProperties(delivery_mode=1),
+#             )
+#             logger.info(f"Отправлено сообщение в очередь -> {self.queue_name}: {asdict(entity)}")
+#         finally:
+#             channel.close()
+#             connection.close()
